14cf81495848eb069f34491af1397cd5251f03bb
guillaume baelen <guillaume.baelen@gmail.com>
2017-10-25 09:44:50 +0200

    Add gold test changes

--- test_gold.m
+++ test_gold.m
@@ -1,7 +1,8 @@
 NbChip = 64;
-nbCode = 2;
+nbCode = 6;
 
-seed = randi([0 1], [1 10]);
+%seed = randi([0 1], [1 10]);
+seed = [ 1 0 0 0 0 0 0 0 0 0 ];
 g1_bit = seed;
 g2_bit = seed;
 
@@ -9,22 +10,23 @@ code = zeros(nbCode, NbChip);
 for i=1:nbCode
     for j=1:NbChip
         %% G1
-        g1_new = mod(1 + g1_bit(3) + g1_bit(10),2);
+        g1_new = mod(g1_bit(3) + g1_bit(10),2);
         g1_out = g1_bit(10);
 
         %shift right
         g1_bit(2:end) = g1_bit(1:end-1);
-        g1_bit(1) = g1_out;
+        g1_bit(1) = g1_new;
 
         %% G2
-        g2_new = mod(1 + g2_bit(2) + g2_bit(3) + g2_bit(6) + g2_bit(8) + g2_bit(9) + g2_bit(10),2);
+        g2_new = mod(g2_bit(2) + g2_bit(3) + g2_bit(6) + g2_bit(8) + g2_bit(9) + g2_bit(10),2);
         g2_out = g2_bit(10);
 
         %shift right
         g2_bit(2:end) = g2_bit(1:end-1);
-        g2_bit(1) = g2_out;
+        g2_bit(1) = g2_new;
 
-        code(i, j) = mod(g1_out + g2_out, 2);
+        %% somme
+        code(i, j) = mod(g1_new + g2_new, 2);
     end
 end
 
@@ -36,5 +38,9 @@ for i=1:nbCode
     end
 end
 
+sum(code(2,:).*code(2,:))/64
+sum(code(3,:).*code(3,:))/64
+sum(code(6,:).*code(4,:))/64
 sum(code(1,:).*code(2,:))/64
-sum(code(1,:).*code(1,:))/64
+sum(code(1,:).*code(3,:))/64
+sum(code(1,:).*code(6,:))/64
